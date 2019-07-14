4795db222d19d7f9134e766546cb81dd84ddba2a
Patrick Roughan <padraig.oruchain@gmail.com>
2019-07-14 13:42:27 +0100

    8ball

---
+++ 8ball.py
@@ -0,0 +1,28 @@
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = raw_input("Ask the magic 8 ball a question: (press enter to quit)")
+
+    answer = random.randint(1,8)
+
+    if question == "":
+        sys.exit()
+    elif answer == 1:
+        print "it is certain"
+    elif answer == 2:
+        print "Outlook good"
+    elif answer == 3:
+        print "You may rely on it"
+    elif answer == 4:
+        print "Ask again later"
+    elif answer == 5:
+        print "Concentrate and ask again"
+    elif answer == 6:
+        print "Reply hazy, try again"
+    elif answer == 7:
+        print "My reply is no"
+    elif answer == 8:
+        print "My sources say no"
